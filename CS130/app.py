80297f48f1f1719cdf6d0b7671d0cd0bf80c5c55
TommyLyon <72283929+TommyLyon@users.noreply.github.com>
2023-10-11 18:20:56 -0400

    Add files via upload

--- CS130/app.py
+++ CS130/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, Response
+from flask_sse import sse
 import threading
 import queue
 import sqlite3
@@ -8,7 +9,8 @@ import json
 import logging
 from threading import Thread
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static') 
+enqueue_count = 0
 new_data = []
 
 # Configure logging
@@ -26,8 +28,8 @@ cursor = conn.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS request_log (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        input INTEGER,
-        result INTEGER,
+        input TEXT,  -- Change data type to TEXT
+        result TEXT,
         timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
 ''')
@@ -35,6 +37,12 @@ cursor.execute('''
 # Commit and close the database connection
 conn.commit()
 conn.close()
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 def process_queue():
     while True:
@@ -44,11 +52,11 @@ def process_queue():
             timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
 
             try:
-                # Log the processed request to the SQLite database as text
-                db_connection = sqlite3.connect('database.db')
+                # Log the processed request to the SQLite database
+                db_connection = sqlite3.connect('request_log.db')
                 db_cursor = db_connection.cursor()
-                db_cursor.execute("INSERT INTO requests (input, result, timestamp) VALUES (?, ?, ?)",
-                                  (str(data), str(result), timestamp))
+                db_cursor.execute("INSERT INTO request_log (input, result, timestamp) VALUES (?, ?, ?)",
+                                  (str(data), result, timestamp))
                 db_connection.commit()
                 db_cursor.close()
                 db_connection.close()
@@ -62,86 +70,142 @@ def generate_updates():
         else:
             time.sleep(1)  # Introduce a delay before checking again
 
-@app.route('/updates')
-def generate_updates():
-    def event_stream():
-        last_id = 0
-        while True:
-            conn = sqlite3.connect('request_log.db')
-            cursor = conn.cursor()
-            cursor.execute('SELECT id, input, result, timestamp FROM request_log WHERE id > ?', (last_id,))
-            data = cursor.fetchall()
-            conn.close()
+# Clear the database upon app startup
+def clear_database():
+    print("Clearing database")  # Add this line for debugging
+    conn = sqlite3.connect('request_log.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM request_log')
+    conn.commit()
+    conn.close()
 
-            for row in data:
-                yield f"data: {json.dumps({'id': row[0], 'input': row[1], 'result': row[2], 'timestamp': row[3]})}\n\n"
-                last_id = row[0]
+def create_table():
+    conn = sqlite3.connect('request_log.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS request_log (
+        id TEXT PRIMARY KEY,
+        input TEXT,
+        result TEXT,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    )''')
+    conn.commit()
+    conn.close()
 
-            time.sleep(1)  # Sleep for 1 second before checking for updates again
-
-    return Response(event_stream(), content_type='text/event-stream')
+# Clear the request log
+@app.route('/clear-log', methods=['POST'])
+def clear_log():
+    conn = sqlite3.connect('request_log.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM request_log')
+    conn.commit()
+    conn.close()
+    return 'Request log cleared', 200
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/log')
-def log():
-    conn = sqlite3.connect('request_log.db')
-    cursor = conn.cursor()
-
-    # Retrieve all processed requests from the table
-    cursor.execute('SELECT input, result, timestamp FROM request_log ORDER BY timestamp DESC')
-    data = cursor.fetchall()
-
-    conn.close()
-
-    # Format the data as a list of dictionaries
-    processed_requests = [{'input': row[0], 'result': row[1], 'timestamp': row[2]} for row in data]
-
-    return jsonify(processed_requests)
+# SQLite database connection pool
+db_connection_pool = sqlite3.connect('request_log.db', check_same_thread=False)
 
 @app.route('/hook', methods=['POST'])
 def hook():
     try:
-        data = int(request.json['integer'])
-        request_queue.put(data)
+        data = request.get_json()
 
-        # Calculate the factorial
-        result = factorial(data)
+        if data is None:
+            return 'Invalid JSON data', 400
 
-        # Insert data into the SQLite table
+        if 'integer' in data:
+            integer = data['integer']
+
+            # Decide whether to store as INTEGER or TEXT based on a threshold
+            if integer <= 2147483647:  # SQLite maximum INTEGER value
+                result = integer
+            else:
+                result = str(integer)
+
+            # Insert data into the SQLite database
+            with db_connection_pool:
+                cursor = db_connection_pool.cursor()
+                cursor.execute("INSERT INTO request_log (input, result, timestamp) VALUES (?, ?, CURRENT_TIMESTAMP)", (integer, result))
+
+            return 'Enqueued', 200
+        else:
+            return 'Missing integer field', 400
+    except Exception as e:
+        app.logger.error(f"Error in /hook: {str(e)}")
+        return 'Internal Server Error', 500
+
+@app.route('/log')
+def log():
+    # Retrieve data from the SQLite database
+    with db_connection_pool:
+        cursor = db_connection_pool.cursor()
+        cursor.execute("SELECT input, timestamp FROM request_log ORDER BY timestamp DESC LIMIT 100")
+        data = cursor.fetchall()
+
+    return render_template('index.html', data=data)
+
+@app.route('/updates')
+def sse_updates():
+    def event_stream():
         conn = sqlite3.connect('request_log.db')
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO request_log (input, result) VALUES (?, ?)', (data, result))
-        conn.commit()
+        cursor.execute('SELECT id, input, result, timestamp FROM request_log ORDER BY timestamp ASC')
+        data = cursor.fetchall()
         conn.close()
 
-        return 'Enqueued', 200
-    except ValueError:
-        app.logger.error('Invalid input. Please enter a valid integer.')
-        return 'Invalid input. Please enter a valid integer.', 400
-    except Exception as e:
-        app.logger.error(f"Error handling request: {str(e)}")
-        return 'Failed to enqueue', 500
+        for row in data:
+            event = {
+                'id': row[0],
+                'input': row[1],
+                'result': row[2],
+                'timestamp': row[3]
+            }
+            yield 'data: ' + json.dumps(event) + '\n\n'
+
+    return Response(event_stream(), content_type='text/event-stream')
+
+
+@app.route('/clear-data', methods=['POST'])
+def clear_data():
+    conn = sqlite3.connect('request_log.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM request_log')
+    conn.commit()
+    conn.close()
+    return 'Data cleared', 200
+
+@app.route('/clear-on-refresh', methods=['GET'])
+def clear_on_refresh():
+    # Clear the request log
+    conn = sqlite3.connect('request_log.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM request_log')
+    conn.commit()
+    conn.close()
+ 
+    return 'Data cleared on page refresh', 200
+
+@app.after_request
+def add_no_cache_header(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+# Implement the factorial function as needed
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
 
 if __name__ == '__main__':
-     
     # Create and start a thread for processing the queue
     processing_thread = threading.Thread(target=process_queue)
     processing_thread.start()
 
-    # Initialize the SQLite database table with TEXT data type for input and result
-    db_connection = sqlite3.connect('database.db')
-    db_cursor = db_connection.cursor()
-    db_cursor.execute('''CREATE TABLE IF NOT EXISTS requests 
-                        (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                         input TEXT, 
-                         result TEXT, 
-                         timestamp TEXT)''')
-    db_connection.commit()
-    db_cursor.close()
-    db_connection.close()
-
     # Start the Flask web application
-    app.run(port=5000)
+    app.run(port=5000, threaded=True)
--- CS130/test.py
+++ CS130/test.py
@@ -1,11 +1,11 @@
 import requests
 import random
 
-# Define the URL of your app's /hook endpoint
+# Define the URL of your Flask application's /hook endpoint
 url = 'http://127.0.0.1:5000/hook'  # Change the URL as needed
 
 # Generate 1000 random integers to simulate the requests
-integers = [random.randint(1, 1000) for _ in range(1000)]
+integers = [random.randint(1, 3) for _ in range(5)]
 
 # Send 1000 POST requests to the /hook endpoint
 for integer in integers:
